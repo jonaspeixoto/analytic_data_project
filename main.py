8d90a42acadad27aeb41f9de3ed47b4e5c13c29c
jonas <jonas.peixoto.087@gmail.com>
2024-09-23 21:39:54 -0300

    Adicionando tabela de id

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import pandas as pd
 import numpy as np
 
-
+# Carregar variáveis de ambiente
 load_dotenv()
 dbname = os.getenv('DB_NAME')
 user = os.getenv('DB_USER')
@@ -12,6 +12,46 @@ password = os.getenv('DB_PASSWORD')
 host = os.getenv('DB_HOST')
 port = os.getenv('DB_PORT')
 
+
+def mapear_df(df):
+    mapeamento_uf = {
+        'Paraná': 'PR',
+        'Paraíba': 'PB',
+        'Tocantins': 'TO',
+        'São Paulo': 'SP',
+        'Piauí': 'PI',
+        'Roraima': 'RR',
+        'Amapá': 'AP',
+        'Rio Grande do Norte': 'RN',
+        'Mato Grosso': 'MT',
+        'Amazonas': 'AM',
+        'Rondônia': 'RO',
+        'Goiás': 'GO',
+        'Mato Grosso do Sul': 'MS',
+        'Espírito Santo': 'ES',
+        'Santa Catarina': 'SC',
+        'Acre': 'AC',
+        'Alagoas': 'AL',
+        'Bahia': 'BA',
+        'Ceará': 'CE',
+        'Rio Grande do Sul': 'RS',
+        'Distrito Federal': 'DF',
+        'Sergipe': 'SE',
+        'Pará': 'PA',
+        'Pernambuco': 'PE',
+        'Minas Gerais': 'MG',
+        'Rio de Janeiro': 'RJ',
+        'Maranhão': 'MA'
+    }
+
+    df['UF'] = df['UF'].map(mapeamento_uf)
+
+    df['Isento'] = df['Isento'].fillna(False).astype(bool)
+
+    df['CEP'] = df['CEP'].fillna('')
+    df['Endereço'] = df['Endereço'].fillna('')
+
+    return df
 
 def inserir_dados(df):
     try:
@@ -25,33 +65,36 @@ def inserir_dados(df):
         cursor = conn.cursor()
         print('Conexão realizada com sucesso')
 
-        # Trantando dados cpf/cnpj
+        # Tratando dados de CPF/CNPJ
         df = df.replace({np.nan: None, pd.NaT: None})
         df['CPF/CNPJ'] = df['CPF/CNPJ'].astype(str)
         df['CPF/CNPJ'] = df['CPF/CNPJ'].str.replace(r'\D', '', regex=True)
 
         duplicados_clientes = []
 
-
-        # Querys
-        insert_query_clientes = f"""
-        INSERT INTO {'tbl_clientes'} (nome_razao_social, nome_fantasia, cpf_cnpj, data_nascimento, data_cadastro) 
+        # Queries
+        insert_query_clientes = """
+        INSERT INTO tbl_clientes (nome_razao_social, nome_fantasia, cpf_cnpj, data_nascimento, data_cadastro) 
         VALUES (%s, %s, %s, %s, %s) ON CONFLICT (cpf_cnpj) DO NOTHING RETURNING id;
         """
 
-        insert_query_planos = f"""
-        INSERT INTO {'tbl_planos'} (descricao, valor) 
+        insert_query_planos = """
+        INSERT INTO tbl_planos (descricao, valor) 
         VALUES (%s, %s) ON CONFLICT (descricao) DO NOTHING RETURNING id;
         """
 
-        insert_query_status = f"""
-        INSERT INTO {'tbl_status_contrato'} (status) 
+        insert_query_status = """
+        INSERT INTO tbl_status_contrato (status) 
         VALUES (%s) ON CONFLICT (status) DO NOTHING RETURNING id;
         """
 
-
+        insert_query_contratos = """
+        INSERT INTO tbl_cliente_contratos (cliente_id, plano_id, dia_vencimento, isento, endereco_logradouro, endereco_numero, endereco_bairro, endereco_cidade, endereco_complemento, endereco_cep, endereco_uf, status_id)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+        """
 
         for indice, row in df.iterrows():
+            # Inserir dados do cliente
             cursor.execute(insert_query_clientes, (
                 row['Nome/Razão Social'],
                 row['Nome Fantasia'],
@@ -60,25 +103,53 @@ def inserir_dados(df):
                 row['Data Cadastro cliente']
             ))
 
-            # Verifica se o cpf já existe na base se sim insere na lista de cliente duplicado
-            if cursor.rowcount == 0:  
-                duplicados_clientes.append(row['CPF/CNPJ'])
+            if cursor.rowcount > 0:  
+                cliente_id = cursor.fetchone()[0]  
+            else:  
+                cursor.execute("SELECT id FROM tbl_clientes WHERE cpf_cnpj = %s;", (row['CPF/CNPJ'],))
+                cliente_id_result = cursor.fetchone()
+                cliente_id = cliente_id_result[0] if cliente_id_result else None
 
+            if cliente_id is None:
+                duplicados_clientes.append(row['CPF/CNPJ'])
 
             # Inserir dados do plano
             cursor.execute(insert_query_planos, (
-            row['Plano'],
-            row['Plano Valor']
+                row['Plano'],
+                row['Plano Valor']
             ))
 
-            # Inserir dados do status
+            cursor.execute("SELECT id FROM tbl_planos WHERE descricao = %s;", (row['Plano'],))
+            plano_id = cursor.fetchone()
+            print(plano_id)
+
+
             cursor.execute(insert_query_status, (
-            row['Status'],
+                row['Status'],
             ))
+
+            cursor.execute("SELECT id FROM tbl_status_contrato WHERE status = %s;", (row['Status'],))
+            status_id = cursor.fetchone()
+            print(status_id)
+
             
+            # Inserir dados na tabela de contratos
+            cursor.execute(insert_query_contratos, (
+                cliente_id,
+                plano_id,
+                row['Vencimento'],
+                row['Isento'],
+                row['Endereço'],
+                row['Número'],
+                row['Bairro'],
+                row['Cidade'],
+                row['Complemento'],
+                row['CEP'],
+                row['UF'],
+                status_id,
+            ))
 
-
-
+        # Commit das transações
         conn.commit()
         cursor.close()
         conn.close()
@@ -86,11 +157,13 @@ def inserir_dados(df):
         print("Dados inseridos com sucesso!")
 
     except Exception as e:
-        print(f"Erro ao conectar : {e}")
+        print(f"Erro ao conectar: {e}")
 
+
+# Carregar dados do Excel e chamar a função
 
 df = pd.read_excel('dados_importacao.xlsx')
+df_mapeado = mapear_df(df)
+print(df_mapeado)
 
-inserir_dados(df)
-
-
+inserir_dados(df_mapeado)
